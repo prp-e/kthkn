fc40b6ebdc48d591a2f628f508be37c31104d56e
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-04-08 19:17:39 +0430

    resolving some errors

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require 'sinatra'
 
 database = SQLite3::Database.open('database.sqlite3')
 database.execute("CREATE TABLE IF NOT EXISTS urls(id INTEGER PRIMARY KEY AUTOINCREMENT, original_url TEXT,short_url TEXT)")
+KEYS = Array.new('a' .. 'z') + Array.new('A'..'Z') + Array.new('0'..'9')
 
 class URLShortener
     def initialize(url, database)
@@ -11,7 +12,6 @@ class URLShortener
     end
 
     def shorten
-        KEYS = Array.new('a' .. 'z') + Array.new('A'..'Z') + Array.new('0'..'9')
         shortened_url_key = Array.new(8) {KEYS.sample}.join
 
         return shortened_url_key
@@ -26,5 +26,7 @@ post '/' do
 end 
 
 get '/:url' do 
-    redirect "http://localhost:4567/" + params[:url]
+    shortener = URLShortener.new(params[:url], database)
+
+    "key is #{shortener.shorten}"
 end 
