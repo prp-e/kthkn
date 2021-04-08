203e6d3d2366c4dc612f376a905e96fd13ef6f57
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-04-08 19:20:09 +0430

    PoC worked

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require 'sinatra'
 
 database = SQLite3::Database.open('database.sqlite3')
 database.execute("CREATE TABLE IF NOT EXISTS urls(id INTEGER PRIMARY KEY AUTOINCREMENT, original_url TEXT,short_url TEXT)")
-KEYS = Array.new('a' .. 'z') + Array.new('A'..'Z') + Array.new('0'..'9')
+KEYS = Array('a' .. 'z') + Array('A'..'Z') + Array('0'..'9')
 
 class URLShortener
     def initialize(url, database)
@@ -26,7 +26,5 @@ post '/' do
 end 
 
 get '/:url' do 
-    shortener = URLShortener.new(params[:url], database)
-
-    "key is #{shortener.shorten}"
+    redirect "http://localhost:4567/#{params[:url]}"
 end 
