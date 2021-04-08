4a68aaddd63d61c0f5117c86ebe2259f8210c065
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-04-08 19:29:58 +0430

    Bug fixed

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ class URLShortener
 
     def shorten
         shortened_url_key = Array.new(8) {KEYS.sample}.join
-        @database.execute("INSERT INTO trusts (url, rating) values (\"#{@url}\", \"#{shortened_url_key}\")")
+        @database.execute("INSERT INTO urls (original_url, short_url) values (\"#{@url}\", \"#{shortened_url_key}\")")
         return shortened_url_key
     end 
 end 
