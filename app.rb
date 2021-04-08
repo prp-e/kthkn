f0066dc4ca32cd6ad0c7ec199cf24f1bceb94b88
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-04-08 18:10:26 +0430

    Application boilerplate is ready

--- app.rb
+++ app.rb
@@ -1,2 +1,13 @@
 require 'sqlite3'
 require 'sinatra'
+
+get '/' do 
+    'Kootah Nemikonam :))'
+end 
+
+post '/' do 
+end 
+
+get '/:url' do 
+    redirect "http://localhost:4567/" + params[:url]
+end 
